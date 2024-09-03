c9d07997a25b3191311ba1f6934e088e49acd598
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 20:54:12 +0200

    eeeee

--- Package.swift
+++ Package.swift
@@ -1,19 +1,18 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "P42-viewmodifiers",
+    platforms: [
+        .iOS(.v14),
+        .watchOS(.v6)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "P42-viewmodifiers",
             targets: ["P42-viewmodifiers"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "P42-viewmodifiers"),
         .testTarget(
--- Sources/P42-viewmodifiers/P42_viewmodifiers.swift
+++ Sources/P42-viewmodifiers/P42_viewmodifiers.swift
@@ -1,2 +1,96 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Modifiers.swift
+//  shops24
+//
+//  Created by Diederick de Buck on 30/11/2022.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct H2: ViewModifier {
+    let labelColor: Color
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(labelColor)
+            .padding()
+
+    }
+}
+
+@available(iOS 13.0, *)
+struct P: ViewModifier {
+    let labelColor: Color
+    func body(content: Content) -> some View {
+        content
+            .font(.body)
+            .foregroundColor(labelColor)
+            .multilineTextAlignment(TextAlignment.leading)
+            .padding(.vertical, 10)
+            .padding(.leading, 30)
+            .padding(.trailing, 30)
+    }
+}
+
+@available(iOS 14.0, *)
+struct BackgroundLabelStyle: LabelStyle {
+    let color: Color
+    let backgroundColor: Color
+    let radius: CGFloat
+    func makeBody(configuration: Configuration) -> some View {
+        Label(configuration)
+            .padding()
+            .foregroundColor(color)
+            .background(backgroundColor)
+            .cornerRadius(radius)
+    }
+}
+
+@available(iOS 13.0, *)
+struct EmptyModifier<Placeholder: View>: ViewModifier {
+    let numberOfItems: Int
+    let placeholder: Placeholder
+    
+    func body(content: Content) -> some View {
+        if numberOfItems != 0 {
+            content
+        } else {
+            placeholder
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+struct EmptyDataModifier<Placeholder: View>: ViewModifier {
+    let items: [Any]
+    let placeholder: Placeholder
+    
+    func body(content: Content) -> some View {
+        if !items.isEmpty {
+            content
+        } else {
+            placeholder
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+struct ButtonPress: ViewModifier {
+    var onPress: () -> Void
+    var onRelease: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ _ in
+                        onPress()
+                    })
+                    .onEnded({ _ in
+                        onRelease()
+                    })
+            )
+    }
+}
