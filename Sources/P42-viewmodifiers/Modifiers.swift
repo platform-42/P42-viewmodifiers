70f9a8195fb015340dda278b2dac000b688d589e
Diederick <diederick.de.buck@platform-42.com>
2025-12-18 13:13:19 +0100

    remove HTML style modifiers

--- Sources/P42-viewmodifiers/Modifiers.swift
+++ Sources/P42-viewmodifiers/Modifiers.swift
@@ -9,40 +9,6 @@ import Foundation
 import SwiftUI
 
 
-public struct H2: ViewModifier {
-    let labelColor: Color
-    
-    public init(labelColor: Color) {
-        self.labelColor = labelColor
-    }
-    
-    public func body(content: Content) -> some View {
-        content
-            .font(.title)
-            .foregroundColor(labelColor)
-            .padding()
-
-    }
-}
-
-public struct P: ViewModifier {
-    let labelColor: Color
-    
-    public init(labelColor: Color) {
-        self.labelColor = labelColor
-    }
-    
-    public func body(content: Content) -> some View {
-        content
-            .font(.body)
-            .foregroundColor(labelColor)
-            .multilineTextAlignment(TextAlignment.leading)
-            .padding(.vertical, 10)
-            .padding(.leading, 30)
-            .padding(.trailing, 30)
-    }
-}
-
 public struct BackgroundLabelStyle: LabelStyle {
     let color: Color
     let backgroundColor: Color
